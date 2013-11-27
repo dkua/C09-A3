267a21df18e00d610265f28f1437fe7205a9dcc2
David Kua <david@kua.io>
2013-11-27 18:42:30 -0500

    Got the search bar to work

--- static/js/tumblog.js
+++ static/js/tumblog.js
@@ -1,15 +1,22 @@
-$("#blog-search").ready(function() {
+$(document).ready(function() {
+  $("input").change(function(event) {
+    var tumblog = $(this).val() + ".tumblr.com";
     var apiKey = "tq73UxKtx9sdgsCvBTmMqrSOHCkqgfppOzoTzoFQLplVxoj4nJ";
-    var tumblog;
     var apiUrl = "http://api.tumblr.com/v2/blog/" + tumblog + "/posts?api_key=" + apiKey;
 
-    });
-function getTumblr(apiUrl) = $.ajax({
+    $.ajax({
       url: apiUrl, 
       type: "GET",
       dataType: "jsonp",
       crossDomain: true,
       success: function(data) {
-        console.log(data);
+        console.log("Looking for posts from " + tumblog);
+        buildPage(data);
       }
-      });
+    });
+  });
+});
+
+function buildPage(data) {
+  console.log(data);
+}
