abbc39a19ff20d06562d0ac16fb51d6c1b1991ae
David Kua <david@kua.io>
2013-11-27 18:08:59 -0500

    Added base files

---
+++ static/js/tumblog.js
@@ -0,0 +1,15 @@
+$("").ready(function() {
+    var apiKey = "tq73UxKtx9sdgsCvBTmMqrSOHCkqgfppOzoTzoFQLplVxoj4nJ";
+    var tumblog;
+    var apiUrl = "http://api.tumblr.com/v2/blog/" + tumblog + "/posts?api_key=" + apiKey;
+
+    });
+function getTumblr(apiUrl) = $.ajax({
+      url: apiUrl, 
+      type: "GET",
+      dataType: "jsonp",
+      crossDomain: true,
+      success: function(data) {
+        console.log(data);
+      }
+      });
