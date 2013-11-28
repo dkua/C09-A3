37f72a39f79bc8e17453a07fb8b07799b0f63522
David Kua <david@kua.io>
2013-11-28 02:18:53 -0500

    Finished the rest of the post types

--- static/js/tumblog.js
+++ static/js/tumblog.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+  $(".loadingDiv").hide().ajaxStart(function() {
+    $(this).show();
+  }).ajaxStop(function() {
+    $(this).hide();
+  });
+
   $("input").change(function(event) {
     var tumblog = $(this).val() + ".tumblr.com";
     var apiKey = "tq73UxKtx9sdgsCvBTmMqrSOHCkqgfppOzoTzoFQLplVxoj4nJ";
@@ -34,34 +40,77 @@ function buildPage(data) {
     for (var i=0; i<posts.length; i++) {
       post = posts[i];
       var tumpost = $("<div />").addClass("tumpost").addClass("center");
-      var postDescription = $("<h3 />").append(post.description);
+      var postDescription = $("<h4 />").append(post.description);
       tumpost.append(postDescription);
 
-      if (post.type === "text") {
-        tumpost.append(post.body);
-      } else if (post.type === "photo") {
-        var photos = post.photos;
+      switch(post.type) {
 
-        var photo;
-        var img;
-        var photoCaption;
-        var photoLink;
-        for (var j=0; j<photos.length; j++) {
-          photo = photos[j];
-          img = $("<img />").attr("src", photo.alt_sizes[3].url);
-          photoLink = $("<a />").attr("href", photo.original_size.url).attr("target", "_blank").append(img);
-          photoCaption =  $("<p />").html(photo.caption);
-          tumpost.append(photoLink, photoCaption);
-        }
-      } else {
-        var otherPost = $("<p />").text("This is a " + post.type + " post");
-        tumpost.append(otherPost);
+        case "text":
+          tumpost.append(post.body);
+          break;
+
+        case "photo":
+          var photos = post.photos;
+
+          var photo;
+          var img;
+          var photoCaption;
+          var photoLink;
+          for (var j=0; j<photos.length; j++) {
+            photo = photos[j];
+            img = $("<img />").attr("src", photo.alt_sizes[0].url);
+            photoLink = $("<a />").attr("href", photo.original_size.url).attr("target", "_blank").append(img);
+            photoCaption =  $("<p />").html(photo.caption);
+            tumpost.append(photoLink, photoCaption);
+          }
+          break;
+
+        case "quote":
+          var quote =  $("<p />").text(post.text);
+          tumpost.append(quote, post.source);
+          break;
+
+        case "link":
+          var titleLink;
+          if (post.title) {
+            titleLink = $("<h2 />").append($("<a />").attr("href", post.url).text(post.title));
+          }
+          tumpost.prepend(titleLink);
+          break;
+
+        case "chat":
+          var chatTitle;
+          if (post.title) {
+            chatTitle = $("<h2 />").text(post.title);
+          }
+          var chatBody = $("<p />").html(post.body.replace(/\r\n/g, "<br />"));
+          tumpost.append(chatTitle, chatBody);
+          break;
+
+        case "audio":
+          var audioTitle = $("<h2 />").text(post.id3_title);
+          tumpost.append(audioTitle, post.player);
+          break;
+
+        case "video":
+          tumpost.append(post.player[2].embed_code);
+          break;
+
+        case "answer":
+          var question = $("<h2 />").html("\"" + post.question + "\" - <em><a href=" + post.asking_url + ">" + post.asking_name + "</a></em>");
+          tumpost.append(question, post.answer);
+          break;
+
+        default:
+          var otherPost = $("<p />").text("This is a " + post.type + " post");
+          tumpost.append(otherPost);
+          break;
       }
 
       var postCaption = post.caption;
       var postURL = $("<a />").attr("href", post.post_url).attr("target", "_blank").text("View post here");
-      var postInfo = $("<p />").text("Posted " + post.date + " by " + post.post_author);
-      tumpost.append(postCaption, postURL, postInfo);
+      var postInfo = $("<p />").text("Posted " + post.date);
+      tumpost.append(postCaption, "<br />",  postURL, postInfo);
       $(".tumblog").append(tumpost);
     }
   } else {
